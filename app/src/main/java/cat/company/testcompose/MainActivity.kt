c27a618fc7848c806d8c32a43ab5e736038da5ab
Carles Company Soler <carlescs@gmail.com>
2022-12-05 17:37:53 +0100

    Add round borders

--- app/src/main/java/cat/company/testcompose/MainActivity.kt
+++ app/src/main/java/cat/company/testcompose/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.camera.core.ExperimentalGetImage
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cat.company.testcompose.bottomSheet.BottomSheetContent
 import cat.company.testcompose.camera.CameraPreview
 import cat.company.testcompose.ui.theme.TestComposeTheme
@@ -33,7 +35,9 @@ class MainActivity : ComponentActivity() {
                     val lastBarcode:MutableState<List<Barcode>?> = remember{mutableStateOf(null)}
                     BottomSheetScaffold(
                         scaffoldState = scaffoldState,
+                        sheetShape = RoundedCornerShape(25.dp,25.dp,0.dp,0.dp),
                         sheetContent = {
+                            if(lastBarcode.value!=null)
                             BottomSheetContent(lastBarcode = lastBarcode)
                         }
                     ) {
