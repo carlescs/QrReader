b80a2dc1f35bd3f91a8368b230fd3d23183c1da9
Carles Company Soler <carlescs@gmail.com>
2022-12-06 19:44:51 +0100

    Add notice!

--- app/src/main/java/cat/company/qrreader/history/History.kt
+++ app/src/main/java/cat/company/qrreader/history/History.kt
@@ -1,14 +1,16 @@
 package cat.company.qrreader.history
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 
 @Composable
 fun History(){
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text(text = "History")
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+        Text(text = "History : Not yet implemented!", modifier = Modifier.align(CenterHorizontally))
     }
 }
