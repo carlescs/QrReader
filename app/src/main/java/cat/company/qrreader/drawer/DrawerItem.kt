322a6eb5d90fc3dfc01eef56c8b94e479e4fae7e
Carles Company Soler <carlescs@gmail.com>
2022-12-06 18:10:11 +0100

    Improve menu colors

--- app/src/main/java/cat/company/qrreader/drawer/DrawerItem.kt
+++ app/src/main/java/cat/company/qrreader/drawer/DrawerItem.kt
@@ -2,13 +2,17 @@ package cat.company.qrreader.drawer
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DrawerState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -18,7 +22,8 @@ import kotlinx.coroutines.launch
 @Composable
 fun DrawerItem(title:String,selected: Boolean,navController:NavController,drawerState: DrawerState,route:String) {
     val coroutineScope = rememberCoroutineScope()
-    val background = if (selected) MaterialTheme.colors.primary else Color.Transparent
+    val background = if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.onPrimary
+    val color = if (selected) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onBackground
     Row(verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
@@ -40,6 +45,7 @@ fun DrawerItem(title:String,selected: Boolean,navController:NavController,drawer
         Text(
             text = title,
             fontSize = 18.sp,
+            color=color,
             modifier = Modifier.padding(horizontal = 10.dp)
         )
     }
