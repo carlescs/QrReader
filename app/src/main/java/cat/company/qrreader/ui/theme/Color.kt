b73bfe22ba1433a57d11637a3af3ceada122c306
Carles Company Soler <carlescs@gmail.com>
2022-12-07 21:57:42 +0100

    Improve theme

--- app/src/main/java/cat/company/qrreader/ui/theme/Color.kt
+++ app/src/main/java/cat/company/qrreader/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package cat.company.qrreader.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Orange200 = Color(0xFFFCC386)
+val Orange500 = Color(0xFFFFAE00)
+val Orange700 = Color(0xFFFF6F00)
 val Teal200 = Color(0xFF03DAC5)
