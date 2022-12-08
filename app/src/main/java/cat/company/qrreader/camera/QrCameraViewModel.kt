135cde455adbfe16a08e26f6d3f6b3f33aca89f5
Carles Company Soler <carlescs@gmail.com>
2022-12-08 09:30:33 +0100

    Improve update of vm

--- app/src/main/java/cat/company/qrreader/camera/QrCameraViewModel.kt
+++ app/src/main/java/cat/company/qrreader/camera/QrCameraViewModel.kt
@@ -5,16 +5,17 @@ import com.google.mlkit.vision.barcode.common.Barcode
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class QrCameraViewModel: ViewModel() {
     private val _uiState= MutableStateFlow(BarcodeState())
     val uiState: StateFlow<BarcodeState> = _uiState.asStateFlow()
 
     fun saveBarcodes(barcodes: List<Barcode>?) {
-        _uiState.value = BarcodeState(barcodes)
+        _uiState.update { it.copy(lastBarcode = barcodes) }
     }
 }
 
 data class BarcodeState(
-    val lastBarcode: List<Barcode>?=null
+    var lastBarcode: List<Barcode>?=null
 )
