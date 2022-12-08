82ea74a40545ff6ab97c109dec512d53d5eb1195
Carles Company Soler <carlescs@gmail.com>
2022-12-08 12:56:56 +0100

    Fix problem of saved barcodes.

--- app/src/main/java/cat/company/qrreader/camera/bottomSheet/BottomSheetContent.kt
+++ app/src/main/java/cat/company/qrreader/camera/bottomSheet/BottomSheetContent.kt
@@ -14,37 +14,43 @@ import cat.company.qrreader.db.BarcodesDb
 import com.google.mlkit.vision.barcode.common.Barcode
 
 @Composable
-fun BottomSheetContent(lastBarcode: List<Barcode>?, db: BarcodesDb){
-    Column(modifier = Modifier
-        .padding(15.dp)
-        .defaultMinSize(minHeight = 250.dp)) {
+fun BottomSheetContent(lastBarcode: List<Barcode>?, db: BarcodesDb) {
+    Column(
+        modifier = Modifier
+            .padding(15.dp)
+            .defaultMinSize(minHeight = 250.dp)
+    ) {
         if (lastBarcode != null) {
-            LazyColumn(modifier = Modifier.fillMaxHeight()){
-                items(items= lastBarcode, itemContent = { barcode ->
-                    Card(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp)
-                        .clickable { },
-                        shape = RoundedCornerShape(5.dp),
-                        elevation = 5.dp) {
-                        Column(modifier = Modifier.padding(15.dp)) {
-                            when (barcode.valueType) {
-                                Barcode.TYPE_URL -> {
-                                    UrlBarcodeDisplay(barcode = barcode, db = db)
-                                }
-                                Barcode.TYPE_CONTACT_INFO -> {
-                                    ContactBarcodeDisplay(barcode=barcode)
-                                }
-                                else -> {
-                                    Title(title = "Other")
-                                    Text(text = barcode.displayValue?: "No")
+            LazyColumn(modifier = Modifier.fillMaxHeight()) {
+                items(
+                    items = lastBarcode,
+                    key={ it.hashCode() },
+                    itemContent = { barcode ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp)
+                                .clickable { },
+                            shape = RoundedCornerShape(5.dp),
+                            elevation = 5.dp
+                        ) {
+                            Column(modifier = Modifier.padding(15.dp)) {
+                                when (barcode.valueType) {
+                                    Barcode.TYPE_URL -> {
+                                        UrlBarcodeDisplay(barcode = barcode, db = db)
+                                    }
+                                    Barcode.TYPE_CONTACT_INFO -> {
+                                        ContactBarcodeDisplay(barcode = barcode)
+                                    }
+                                    else -> {
+                                        Title(title = "Other")
+                                        Text(text = barcode.displayValue ?: "No")
+                                    }
                                 }
                             }
                         }
-                    }
-                })
+                    })
             }
-
         }
     }
 }
