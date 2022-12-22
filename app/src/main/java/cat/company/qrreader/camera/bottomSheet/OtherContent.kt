42c32c59f34f6c659a114a19a7d4477c4ed7f93b
Carles Company Soler <carlescs@gmail.com>
2022-12-22 10:07:16 +0100

    Add product search

--- app/src/main/java/cat/company/qrreader/camera/bottomSheet/OtherContent.kt
+++ app/src/main/java/cat/company/qrreader/camera/bottomSheet/OtherContent.kt
@@ -2,12 +2,19 @@ package cat.company.qrreader.camera.bottomSheet
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import cat.company.qrreader.db.BarcodesDb
 import cat.company.qrreader.db.entities.SavedBarcode
@@ -18,10 +25,28 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun OtherContent(barcode: Barcode, db: BarcodesDb){
+    val uriHandler = LocalUriHandler.current
     val coroutineScope= CoroutineScope(Dispatchers.IO)
     val saved = remember{ mutableStateOf(false) }
-    Title(title = "Other")
-    Text(text = barcode.displayValue ?: "No")
+    Title(title = if (barcode.format==Barcode.FORMAT_EAN_13) "EAN13" else "Other")
+    if(barcode.format==Barcode.FORMAT_EAN_13) {
+        ClickableText(text = buildAnnotatedString {
+            this.withStyle(
+                SpanStyle(
+                    color = Color.Blue,
+                    textDecoration = TextDecoration.Underline
+                )
+            ) {
+                append(barcode.displayValue ?: "No")
+            }
+        }, onClick = {
+            if (barcode.displayValue != null)
+                uriHandler.openUri("https://www.google.com/search?q=${barcode.displayValue!!}&tbm=shop")
+        })
+    }
+    else{
+        Text(text = barcode.displayValue ?: "No")
+    }
     Spacer(modifier = Modifier.height(20.dp))
     TextButton(onClick = {
         coroutineScope.launch { db.savedBarcodeDao().insertAll(SavedBarcode(type = barcode.valueType, barcode = barcode.displayValue!!)) }
