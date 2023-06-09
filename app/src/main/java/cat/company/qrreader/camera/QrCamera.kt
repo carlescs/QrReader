44e2927746f3dc083c7b6042c6971b7ff3617519
Carles Company Soler <carlescs@gmail.com>
2023-06-09 20:48:47 +0200

    Fix bottom sheet display

--- app/src/main/java/cat/company/qrreader/camera/QrCamera.kt
+++ app/src/main/java/cat/company/qrreader/camera/QrCamera.kt
@@ -15,10 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,7 +34,6 @@ import kotlinx.coroutines.launch
 fun QrCamera(db: BarcodesDb, snackbarHostState: SnackbarHostState, viewModel: QrCameraViewModel = QrCameraViewModel()){
     val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
     val coroutineScope = rememberCoroutineScope()
-    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
     val bottomSheetState = rememberModalBottomSheetState()
     val state by viewModel.uiState.collectAsState()
 
@@ -47,8 +43,9 @@ fun QrCamera(db: BarcodesDb, snackbarHostState: SnackbarHostState, viewModel: Qr
     Column(modifier=Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.Center){
-        if(!permissionState.status.isGranted)
+        if(!permissionState.status.isGranted) {
             permissionState.launchPermissionRequest()
+        }
         else{
             CameraPreview {
                 if (it?.isNotEmpty() == true)
@@ -56,7 +53,6 @@ fun QrCamera(db: BarcodesDb, snackbarHostState: SnackbarHostState, viewModel: Qr
                         viewModel.saveBarcodes(it)
                         coroutineScope.launch {
                             bottomSheetState.show()
-                            openBottomSheet=true
                         }
                     }
             }
@@ -64,24 +60,21 @@ fun QrCamera(db: BarcodesDb, snackbarHostState: SnackbarHostState, viewModel: Qr
         BackHandler(enabled = bottomSheetState.isVisible) {
             coroutineScope.launch {
                 bottomSheetState.hide()
-                openBottomSheet=false
             }
         }
-        if(openBottomSheet) {
+        if(bottomSheetState.isVisible) {
             ModalBottomSheet(
                 shape = RoundedCornerShape(25.dp, 25.dp, 0.dp, 0.dp),
-                content = {
-                    BottomSheetContent(lastBarcode = state.lastBarcode, db = db, snackbarHostState)
-                },
-                sheetState = bottomSheetState,
+//                sheetState = bottomSheetState,
                 onDismissRequest = {
                     coroutineScope.launch {
                         bottomSheetState.hide()
-                        openBottomSheet=false
                     }
                 },
                 scrimColor = Color.DarkGray.copy(alpha = 0.8f)
-            )
+            ){
+                BottomSheetContent(lastBarcode = state.lastBarcode, db = db, snackbarHostState)
+            }
         }
     }
 }
