57a486d76761b11aa94f4ca61297a3cb61eeed95
Carles Company Soler <carlescs@gmail.com>
2023-09-23 20:16:13 +0200

    Update versions

--- app/src/main/java/cat/company/qrreader/camera/QrCamera.kt
+++ app/src/main/java/cat/company/qrreader/camera/QrCamera.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -25,6 +27,7 @@ import cat.company.qrreader.db.BarcodesDb
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
@@ -44,7 +47,23 @@ fun QrCamera(db: BarcodesDb, snackbarHostState: SnackbarHostState, viewModel: Qr
         horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.Center){
         if(!permissionState.status.isGranted) {
-            permissionState.launchPermissionRequest()
+            Column {
+                val textToShow = if (permissionState.status.shouldShowRationale) {
+                    // If the user has denied the permission but the rationale can be shown,
+                    // then gently explain why the app requires this permission
+                    "The camera is important for this app. Please grant the permission."
+                } else {
+                    // If it's the first time the user lands on this feature, or the user
+                    // doesn't want to be asked again for this permission, explain that the
+                    // permission is required
+                    "Camera permission required for this feature to be available. " +
+                            "Please grant the permission"
+                }
+                Text(textToShow)
+                Button(onClick = { permissionState.launchPermissionRequest() }) {
+                    Text("Request permission")
+                }
+            }
         }
         else{
             CameraPreview {
