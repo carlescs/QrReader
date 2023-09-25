e4e1c49220a65002640e62cf744c2c1df8939e63
Carles Company Soler <carlescs@gmail.com>
2023-09-25 22:06:04 +0200

    Add title to edit dialog

--- app/src/main/java/cat/company/qrreader/history/EditBarcodeDialog.kt
+++ app/src/main/java/cat/company/qrreader/history/EditBarcodeDialog.kt
@@ -9,11 +9,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import cat.company.qrreader.db.BarcodesDb
 import cat.company.qrreader.db.entities.SavedBarcode
@@ -36,6 +41,9 @@ fun EditBarcodeDialog(savedBarcode:SavedBarcode, db: BarcodesDb, onRequestClose:
                 contentAlignment = Alignment.Center
             ) {
                 Column(modifier = Modifier.padding(20.dp)) {
+                    Text(text = "Edit barcode",
+                        fontSize = 20.sp,
+                        )
                     TextField(modifier = Modifier.padding(vertical = 5.dp), value = text, singleLine = true, onValueChange = {text=it}, label = { Text(
                         text = "Title"
                     )})
