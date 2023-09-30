dbdd1c794faca2a5856157b25d284ecd7e6eacc6
Carles Company Soler <carlescs@gmail.com>
2023-09-30 15:19:20 +0200

    Try fixing auto refresh History

--- app/src/main/java/cat/company/qrreader/history/HistoryViewModel.kt
+++ app/src/main/java/cat/company/qrreader/history/HistoryViewModel.kt
@@ -1,14 +1,22 @@
 package cat.company.qrreader.history
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import cat.company.qrreader.db.BarcodesDb
 import cat.company.qrreader.db.entities.SavedBarcode
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
 class HistoryViewModel(val db: BarcodesDb):ViewModel() {
-    lateinit var savedBarcodes:Flow<List<SavedBarcode>>
+    private val TIMEOUT_MILLIS: Long = 5000
+
+    lateinit var savedBarcodes:StateFlow<List<SavedBarcode>>
 
     fun loadBarcodes(){
         savedBarcodes=db.savedBarcodeDao().getAll()
+            .stateIn(viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = emptyList())
     }
 }
