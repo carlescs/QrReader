9cb92afc26c8900fc99b232c301207d2aacd9688
Carles Company Soler <carlescs@gmail.com>
2023-10-09 21:53:26 +0200

    Improve theming

--- app/src/main/java/cat/company/qrreader/ui/theme/Theme.kt
+++ app/src/main/java/cat/company/qrreader/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package cat.company.qrreader.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val lightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -89,6 +94,14 @@ fun QrReaderTheme(isDarkTheme: Boolean = isSystemInDarkTheme(), isDynamicColor:B
 
         isDarkTheme -> darkColors
         else -> lightColors
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.surface.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !isDarkTheme
+        }
     }
 
     MaterialTheme(
