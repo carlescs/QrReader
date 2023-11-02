3af51117f97b5274cf73899f4bc367a19683603e
Carles Company Soler <carlescs@gmail.com>
2023-11-02 17:48:11 +0100

    Improve padding for tags.

--- app/src/main/java/cat/company/qrreader/history/Tag.kt
+++ app/src/main/java/cat/company/qrreader/history/Tag.kt
@@ -1,6 +1,8 @@
 package cat.company.qrreader.history
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -18,7 +20,10 @@ import cat.company.qrreader.utils.Utils
 fun Tag(it: Tag) {
     val color = Utils.parseColor(it.color)
     Card(
-        modifier = Modifier.padding(5.dp),
+        modifier = Modifier.padding(5.dp)
+            .wrapContentHeight()
+            .wrapContentWidth()
+            .padding(5.dp),
         colors = if (color != null) CardDefaults.cardColors(
             containerColor = color
         ) else CardDefaults.cardColors(),
