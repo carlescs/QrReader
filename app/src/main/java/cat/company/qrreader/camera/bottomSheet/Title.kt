59d27cef39cdf70f3b80581caebfcae712f75611
Carles Company Soler <carles.cs@outlook.com>
2025-10-12 18:19:10 +0000

    Merged PR 737: feat: Update app version and adjust bottom sheet title style
    feat: Update app version and adjust bottom sheet title style
    
    - Update app version to 3.12.0 (versionCode 234).
    - Modify the bottom sheet title by reducing the font size to 18.sp and applying a bold font weight for better visual hierarchy.

--- app/src/main/java/cat/company/qrreader/camera/bottomSheet/Title.kt
+++ app/src/main/java/cat/company/qrreader/camera/bottomSheet/Title.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -13,6 +14,6 @@ import androidx.compose.ui.unit.sp
  */
 @Composable
 fun Title(title:String){
-    Text(text = title, fontSize = 25.sp)
+    Text(text = title, fontSize = 18.sp, fontWeight = FontWeight.Bold)
     Spacer(modifier = Modifier.height(10.dp))
 }
